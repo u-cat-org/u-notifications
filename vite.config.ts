dca419b72e6e9a11decec0720b93f19f7a5e4077
opoltoratskyi <oleks.poltoratskyi@icloud.com>
2024-06-07 21:36:57 +0200

    Tweak library build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    }
+      formats: [ 'es' ]
+    },
+    copyPublicDir: false
   }
 })
