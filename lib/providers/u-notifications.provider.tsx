9233e1fe7cf8002ee5e5688ffa3402dc91645eac
opoltoratskyi <oleks.poltoratskyi@icloud.com>
2024-06-21 20:38:01 +0200

    [Bugfix] Add notification type API missed.

--- lib/providers/u-notifications.provider.tsx
+++ lib/providers/u-notifications.provider.tsx
@@ -8,6 +8,7 @@ interface INotificationContext {
   addNotification: (color: UNotificationColor, text: string) => void;
   primary: (text: string) => void;
   secondary: (text: string) => void;
+  warning: (text: string) => void;
   danger: (text: string) => void;
   dark: (text: string) => void;
   success: (text: string) => void;
@@ -20,6 +21,7 @@ const DEFAULT_NOTIFICATIONS_CONTEXT: INotificationContext = {
   addNotification: () => undefined,
   primary: () => undefined,
   secondary: () => undefined,
+  warning: () => undefined,
   danger: () => undefined,
   dark: () => undefined,
   success: () => undefined,
@@ -67,6 +69,10 @@ export const UNotificationsProvider = ({ children }: React.PropsWithChildren) =>
     addNotification(UNotificationColor.info, text);
   }
 
+  function warning(text: string): void {
+    addNotification(UNotificationColor.warning, text);
+  }
+
 
   function removeNotification(index: number): void {
     // @ts-ignore
@@ -74,7 +80,7 @@ export const UNotificationsProvider = ({ children }: React.PropsWithChildren) =>
   }
 
   return <NotificationsContext.Provider
-    value={ { addNotification, primary, secondary, danger, dark, success, light, info } }>
+    value={ { addNotification, primary, secondary, warning, danger, dark, success, light, info } }>
     { children }
     <UNotificationsContainer notifications={ notifications }
                              onToggleHandler={ removeNotification }></UNotificationsContainer>
